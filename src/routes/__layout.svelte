33dbe50ea27116bb09ee771cc61d50970a6aa821
Carey Estes <carey@careyestes.com>
2022-01-11 13:15:09 -0600

    Adding SMUI lib

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,18 +1,54 @@
-<script>
+<script lang="ts">
   import "../app.css";
+  import TopAppBar, {
+    Row,
+    Section,
+    Title,
+    AutoAdjust,
+    TopAppBarComponentDev,
+  } from '@smui/top-app-bar';
+  import IconButton from '@smui/icon-button';
+  import LoremIpsum from '$lib/LoremIpsum.svelte';
+
+  let topAppBar: TopAppBarComponentDev;
 </script>
 
-<nav class="flex flex-row">
-  <button class="flex flex-col p-4 basis-10 bg-blue hover:bg-blue-light text-white fill-white">
-    <svg viewBox="0 0 40 17">
-      <rect width="40" height="6"/>
-      <rect y="11" width="40" height="6"/>
-    </svg>
-    <span class="p-1 text-xs">MENU</span>
-  </button>
-  <section>
-    <p class="text-sm font-bold p-2"><a href="">HOME</a> » </p>
-  </section>
-</nav>
+<TopAppBar bind:this={topAppBar} variant="short" collapsed>
+  <Row>
+    <Section>
+      <IconButton class="material-icons">menu</IconButton>
+      <Title>Short</Title>
+    </Section>
+    <Section align="end" toolbar>
+      <IconButton class="material-icons" aria-label="Download"
+        >file_download</IconButton
+      >
+    </Section>
+  </Row>
+</TopAppBar>
+<AutoAdjust {topAppBar}>
+  <h5>Short, Always Collapsed</h5>
+  <LoremIpsum />
+  <img
+    alt="Page content placeholder"
+    src="/page-content.jpg"
+    style="display: block; max-width: 100%; height: auto; margin: 1em auto;"
+  />
+</AutoAdjust>
+
+<style>
+  /* Hide everything above this component. */
+  :global(app),
+  :global(body),
+  :global(html) {
+    display: block !important;
+    height: auto !important;
+    width: auto !important;
+    position: static !important;
+  }
+</style>
+
+
+
 
 <slot />
--- src/routes/accounts.svelte
+++ src/routes/accounts.svelte
@@ -1,5 +1,13 @@
+<script>
+	import {accounts} from "../stores/mockend";
+	console.log($accounts);
+</script>
 <svelte:head>
 	<title>Accounts</title>
 </svelte:head>
 
 <h1>This will have the list of accounts</h1>
+
+{#each $accounts as account}
+<p>{account.name}</p>
+{/each}
